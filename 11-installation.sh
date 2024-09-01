25f4f9fd49968d7a3388177b4617bfcb97053d9a
anjaneyareddym <anjaneyareddym@gmail.com>
2024-09-01 16:49:25 +0530

    functions

---
+++ 11-installation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID ne 0 ]
+
+then
+  echo "Please run this script root priveleges"
+  exit 1
+fi  
+   
+dnf list installed git
+
+if [ $? -ne 0]  
+then 
+  echo "Git is not installed, going to install it .."
+    dnf install git -y
+ else
+     echo "Git is alredy installed, nothing to do"  
+fi
