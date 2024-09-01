b5affb4bec9a13953e8560e95eead11f40f01f59
anjaneyareddym <anjaneyareddym@gmail.com>
2024-09-01 11:32:28 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Ramesh : Hi Suresh, How are you?"
+echo "Suresh : Hi Ramesh , I am fine .How are you?"
+echo "Ramesh :I am fine,What is doning?"
+echo "Suresh : I am learning Devops"
