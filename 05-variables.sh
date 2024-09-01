cf2a8b44453f89922448f91472f89b8e718f1535
anjaneyareddym <anjaneyareddym@gmail.com>
2024-09-01 12:02:37 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,6 +6,7 @@ read -s USERNAME
 
 echo "Username enter name $USERNAME"
 
+echo " Please enter Username ::"
 read -s PASSWORD
 
 echo "Password enter name $PASSWORD"
---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FRUITS=("APPLE" "KIWI" "ORANGE")
+
+echo "First fruit is :${FRUITS[0]}"
+echo "Second fruit is :${FRUITS[0]}"
+echo "Third fruit is :${FRUITS[0]}"
+
+echo "All fruit is :${FRUITS[@]}"
