deba180e7e6c8680a1f7315ac00eed0b6aa44ee0
anjaneyareddym <anjaneyareddym@gmail.com>
2024-09-01 15:56:48 +0530

    added files

---
+++ 09-special-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " All varibiles passed to the script :$@"
+
+echo "Number of varibiles passed :$#"
+
+echo "Script name  $0"
+
+echo "current working directory $PWD"
+echo "Home directory of :$HOME"
+echo " PID of   $$"
