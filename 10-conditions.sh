9292d23d9d858a13d7dd9a164d353f06fe3d6712
anjaneyareddym <anjaneyareddym@gmail.com>
2024-09-01 16:13:17 +0530

    modified file

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,7 +5,7 @@ NUMBER =$1
 
 if [$NUMBER -gt 20 ]
 then
-   echo "Given number :$Number is gretter than 20"
+   echo "Given number :$NUMBER is gretter than 20"
 else   
-   echo "Given number :$Number is lessthan than 20"
+   echo "Given number :$NUMBER is lessthan than 20"
 if   
