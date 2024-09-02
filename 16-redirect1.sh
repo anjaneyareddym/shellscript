e9a10978bb74fe521a81913b08d46f839c459fa2
anjaneyareddym <anjaneyareddym@gmail.com>
2024-09-02 15:02:14 +0530

    add functions

--- 16-redirect1.sh
+++ 16-redirect1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u) #
-LOGS_FOLDER="var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #$0 file name
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
